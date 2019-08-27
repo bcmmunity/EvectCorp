e03ec65f522c57312350867e94d1ef0ebfb406eb
Artem <moranmr8@gmail.com>
2019-08-27 16:01:51 +0300

    added start files

---
+++ EvectCorp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EvectCorp.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+    }
+}
